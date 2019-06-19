75730e4de60ed82a102dc6595407491d12040238
Rasheed Preston <rasheed.preston@gmail.com>
2019-06-18 20:40:33 -0500

    add actions for adding and removing ratings

--- app/containers/ScorePage/actions.js
+++ app/containers/ScorePage/actions.js
@@ -1,4 +1,11 @@
-import { SUBMIT_RATING, SET_RATING, SET_NAME } from './constants';
+import uuid from 'uuid/v1';
+import {
+  SUBMIT_RATING,
+  SET_RATING,
+  SET_NAME,
+  ADD_RATING,
+  REMOVE_RATING,
+} from './constants';
 
 export const onSubmitForm = value => ({
   type: SUBMIT_RATING,
@@ -13,4 +20,20 @@ export const setRating = value => ({
 export const setName = value => ({
   type: SET_NAME,
   payload: { value },
+});
+
+// DO NOT CHANGE - e.g.: do not add or remove parameters
+export const addRating = (id = uuid()) => ({
+  type: ADD_RATING,
+  payload: {
+    id,
+  },
+});
+
+// DO NOT CHANGE - e.g.: do not add or remove parameters
+export const removeRating = id => ({
+  type: REMOVE_RATING,
+  payload: {
+    id,
+  },
 });
--- app/containers/ScorePage/constants.js
+++ app/containers/ScorePage/constants.js
@@ -3,3 +3,5 @@ export const key = 'score';
 export const SUBMIT_RATING = 'SUBMIT_RATING - this action submits rating';
 export const SET_RATING = 'SET_RATING';
 export const SET_NAME = 'SET_NAME';
+export const ADD_RATING = 'Submit a new rating';
+export const REMOVE_RATING = 'Remove an existing rating';
